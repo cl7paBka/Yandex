ee7c6ca36b9d6a959ed1859ed729db4a19c258ec
cl7paBka <maks-kandikov@yandex.ru>
2024-03-12 15:16:32 +0300

    nice try

--- Algorithms 5/Lesson #1/A. Покраска деревьев.py
+++ Algorithms 5/Lesson #1/A. Покраска деревьев.py
@@ -1,1 +1,24 @@
-#начни это, бро)
+P, V = map(int, input().split()) #P - номер дерева Васи, V - на сколько деревьев вася может отдалиться
+Q, M = map(int, input().split()) #Q - номер дерева Маши, M - на сколько деревьев маша может отдалиться
+
+vasya_min = P-V
+vasya_max = P+V
+maria_min = Q-M
+maria_max = Q+M
+
+trees_count = abs(vasya_max - vasya_min) + abs(maria_max - maria_min) + 2
+
+vasya_pointer = vasya_min
+maria_pointer = maria_min
+
+while vasya_pointer <= vasya_max and maria_pointer <= maria_max:
+    if vasya_pointer == maria_pointer:
+        trees_count -= 1
+        vasya_pointer += 1
+        maria_pointer += 1
+    elif vasya_pointer > maria_pointer:
+        maria_pointer += 1
+    else:
+        vasya_pointer += 1
+print(trees_count)
+
